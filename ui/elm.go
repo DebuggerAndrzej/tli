b428dfa2846fe67d417599a6588768e89145997c
DebuggerAndrzej <redpandaworkstation@gmail.com>
2024-03-13 19:45:12 +0100

    Elm file cleanup. Changed a bit message passing

--- ui/elm.go
+++ ui/elm.go
@@ -3,83 +3,29 @@ package ui
 import (
 	"fmt"
 
-	"github.com/charmbracelet/bubbles/viewport"
 	tea "github.com/charmbracelet/bubbletea"
-	"github.com/charmbracelet/lipgloss"
 )
 
 const useHighPerformanceRenderer = false
 
 type updatedContents string
-type requiresFiltersUpdate bool
-type clearAllFilters bool
 
 func (m model) Init() tea.Cmd {
 	return nil
 }
 
 func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
-	var (
-		cmd  tea.Cmd
-		cmds []tea.Cmd
-	)
-
 	switch msg := msg.(type) {
 	case tea.KeyMsg:
-		if m.textInput.Focused() {
-			return m.handleTextInput(msg)
-		}
-		switch keypress := msg.String(); keypress {
-		case "q":
-			return m, tea.Quit
-		case "f":
-			m.textInput.Focus()
-			return m, nil
-		case "r":
-			return m, tea.Cmd(m.clearFilters)
-		}
-
-		if k := msg.String(); k == "ctrl+c" || k == "q" || k == "esc" {
-			return m, tea.Quit
-		}
-
+		return m.handleKeyInput(msg)
+	case tea.MouseMsg:
+		return m.viewPortUpdate(msg)
 	case tea.WindowSizeMsg:
-		headerHeight := lipgloss.Height(m.headerView())
-		footerHeight := lipgloss.Height(m.footerView())
-		verticalMarginHeight := headerHeight + footerHeight
-
-		if !m.ready {
-			m.viewport = viewport.New(msg.Width, msg.Height-verticalMarginHeight)
-			m.viewport.YPosition = headerHeight
-			m.viewport.HighPerformanceRendering = useHighPerformanceRenderer
-			m.ready = true
-			m.viewport.YPosition = headerHeight + 1
-			cmds = append(cmds, m.updateViewportContent)
-		} else {
-			m.viewport.Width = msg.Width
-			m.viewport.Height = msg.Height - verticalMarginHeight
-		}
-		if useHighPerformanceRenderer {
-			cmds = append(cmds, viewport.Sync(m.viewport))
-		}
+		return m.handleWindowSizeMsg(msg)
 	case updatedContents:
 		m.viewport.SetContent(string(msg))
-		m.textInput.Blur()
-		m.textInput.Reset()
-	case requiresFiltersUpdate:
-		m.filters = append(m.filters, m.textInput.Value())
-		return m, tea.Cmd(m.updateViewportContent)
-	case clearAllFilters:
-		if len(m.filters) == 0 {
-			return m, nil
-		}
-		m.filters = nil
-		return m, tea.Cmd(m.updateViewportContent)
 	}
-	m.viewport, cmd = m.viewport.Update(msg)
-	cmds = append(cmds, cmd)
-
-	return m, tea.Batch(cmds...)
+	return m, nil
 }
 
 func (m model) View() string {
--- ui/model.go
+++ ui/model.go
@@ -22,6 +22,10 @@ type model struct {
 	filters    []string
 }
 
+func initModel() model {
+	return model{logEntries: backend.LoadFile("logs/test2.log"), textInput: textinput.New()}
+}
+
 func (m model) headerView() string {
 	return mainTitleStyle.Width(m.viewport.Width).Render("TLI")
 }
@@ -33,29 +37,72 @@ func (m model) footerView() string {
 	return footerStyle.Width(m.viewport.Width).Render(fmt.Sprintf("%3.f%%", m.viewport.ScrollPercent()*100))
 }
 
-func (m model) handleTextInput(msg tea.KeyMsg) (tea.Model, tea.Cmd) {
-	if msg.String() == "enter" {
-		if m.textInput.Value() == "" {
+func (m model) handleKeyInput(msg tea.KeyMsg) (tea.Model, tea.Cmd) {
+	switch m.textInput.Focused() {
+	case true:
+		if msg.String() == "enter" {
+			inputValue := m.textInput.Value()
 			m.textInput.Blur()
-			return m, nil
+			m.textInput.Reset()
+			if inputValue == "" {
+				return m, nil
+			}
+			m.filters = append(m.filters, inputValue)
+			return m, tea.Cmd(m.updateViewportContent)
 		}
-		return m, tea.Cmd(m.updateFilters)
+		var cmd tea.Cmd
+		m.textInput, cmd = m.textInput.Update(msg)
+		return m, cmd
+	case false:
+		switch keypress := msg.String(); keypress {
+		case "q":
+			return m, tea.Quit
+		case "f":
+			m.textInput.Focus()
+			return m, nil
+		case "r":
+			return m.clearFilters()
+		default:
+			return m.viewPortUpdate(msg)
+		}
 	}
+	return m, nil
+}
+
+func (m model) viewPortUpdate(msg tea.Msg) (tea.Model, tea.Cmd) {
 	var cmd tea.Cmd
-	m.textInput, cmd = m.textInput.Update(msg)
+	m.viewport, cmd = m.viewport.Update(msg)
 	return m, cmd
 }
 
-func initModel() model {
-	return model{logEntries: backend.LoadFile("logs/test2.log"), textInput: textinput.New()}
+func (m model) handleWindowSizeMsg(msg tea.WindowSizeMsg) (tea.Model, tea.Cmd) {
+	var cmds []tea.Cmd
+	headerHeight := lipgloss.Height(m.headerView())
+	footerHeight := lipgloss.Height(m.footerView())
+	verticalMarginHeight := headerHeight + footerHeight
+	if !m.ready {
+		m.viewport = viewport.New(msg.Width, msg.Height-verticalMarginHeight)
+		m.viewport.YPosition = headerHeight
+		m.viewport.HighPerformanceRendering = useHighPerformanceRenderer
+		m.ready = true
+		m.viewport.YPosition = headerHeight + 1
+		cmds = append(cmds, m.updateViewportContent)
+	} else {
+		m.viewport.Width = msg.Width
+		m.viewport.Height = msg.Height - verticalMarginHeight
+	}
+	if useHighPerformanceRenderer {
+		cmds = append(cmds, viewport.Sync(m.viewport))
+	}
+	return m, tea.Batch(cmds...)
 }
 
-func (m model) updateFilters() tea.Msg {
-	return requiresFiltersUpdate(true)
-}
-
-func (m model) clearFilters() tea.Msg {
-	return clearAllFilters(true)
+func (m model) clearFilters() (tea.Model, tea.Cmd) {
+	if len(m.filters) == 0 {
+		return m, nil
+	}
+	m.filters = nil
+	return m, tea.Cmd(m.updateViewportContent)
 }
 
 func (m model) updateViewportContent() tea.Msg {
